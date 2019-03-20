5db2e2a121563085daed81e51776652dabcc6aa3
onisoralex <onisor_alex@gmx.at>
2019-03-20 03:44:24 +0100

    Refactored basic functions

--- HTML Project Lyrics/src/LyricsProcessing.js
+++ HTML Project Lyrics/src/LyricsProcessing.js
@@ -59,67 +59,6 @@ function getParts(complete_song_array, starting_positions_of_parts) {
 
 	return parts2;
 }
-
-function getSongTitle(complete_lyrics_array3, start_position_of_first_part) {
-	let title = "";
-
-	for (let i = 0; i < start_position_of_first_part; i++) {	//From Beginning of Song to the end of the Title Part (beginning of first Song Part
-		if (complete_lyrics_array3[i] !== "") {
-			title = complete_lyrics_array3[i];
-		}
-		if (title != "") break;
-	}
-
-	return title;
-}
-
-function getDefaultSongKey(complete_lyrics_array4, start_position_of_first_part) {
-	let key = "";
-
-	for (let i = 0; i < start_position_of_first_part; i++) {	//From Beginning of Song to the end of the Title Part (beginning of first Song Part
-		if (complete_lyrics_array4[i] !== "") {
-			let keyline = complete_lyrics_array4[i].split(":");
-			if (keyline.length = 2) {
-				if (keyline[0].toLowerCase() === "key") {
-					key = keyline[1].replace(" ", "");
-				}
-			}
-		}
-		if (key != "") break;
-	}
-
-	return key;
-}
-
-function getArtist(complete_lyrics_array5, start_position_of_first_part) {
-	let artist = "";
-
-	for (let i = 0; i < start_position_of_first_part; i++) {	//From Beginning of Song to the end of the Title Part (beginning of first Song Part
-		if (complete_lyrics_array5[i] !== "") {
-			let artistline = complete_lyrics_array5[i].split(":");
-			if (artistline.length = 2) {
-				let artist_lowercase = artistline[0].toLowerCase();
-				if (artist_lowercase === "artist" || artist_lowercase === "interpret") {
-					key = artistline[1].replace(" ", "");
-				}
-			}
-		}
-		if (artist != "") break;
-	}
-
-	return key;
-}
-
-function getDefaultSongStructure(parts4) {
-	let structure = "";
-
-	for (let i = 0; i < parts4.length - 2; i++) {	//Minus 2 to exclude the Info Part
-		structure = structure + parts4[i].name + ", ";
-	}
-	structure = structure.concat(parts4[parts4.length - 2].name);
-
-	return structure;
-}
 //-----
 
 function processSong(parts3) {
