57bf219c95c2b1bc139f20439f7460061b046e4c
onisoralex <onisor_alex@gmx.at>
2019-03-29 10:26:47 +0100

    Added functions to the table planner

--- Random Tests and Stuff/Sitzplangestalter/script.js
+++ Random Tests and Stuff/Sitzplangestalter/script.js
@@ -15,6 +15,25 @@ function addNewPerson() {
   personlist.push(new Person(firstname.value, lastname.value));
   firstname.value = "";
   lastname.value = "";
+
+  showPersonList();
+}
+
+// Deletes a person from the personlist
+function deletePerson() {
+  let personToDelete = document.getElementById("personid");
+
+  console.log("going to delete person: " + personToDelete);
+
+  personlist.splice(personToDelete, 1);
+
+  showPersonList();
+}
+
+//TODO
+// Edit person in personlist
+function editPerson() {
+
 }
 
 // Clicking on the button creates a link which can be clicked to download the file with all the settings
@@ -36,8 +55,13 @@ function showPersonList() {
   plist.innerHTML = "";
 
   personlist.forEach(element => {
-    list += `<tr><td>${element.firstname}</td><td>${element.lastname}</td><td>${(element.mature) ? "matur" : "copil"}</td></tr>`;
+    list += `<tr><td>${element.firstname}</td><td>${element.lastname}</td></tr>`;
   });
 
   plist.innerHTML = list;
+}
+
+// Prints the array to the console
+function showPersonArray() {
+  console.log(personlist);
 }
