be14035dd1fe3ce2c926a907c3bde114cde54f68
onisoralex <onisor_alex@gmx.at>
2019-04-04 20:01:16 +0200

    Append drop at first valid point

--- Random Tests and Stuff/Drag'n'Drop/script.js
+++ Random Tests and Stuff/Drag'n'Drop/script.js
@@ -8,13 +8,17 @@ function drag(ev) {
 
 function drop(ev) {
   ev.preventDefault();
+  // Get the data to be transferred
   var data = ev.dataTransfer.getData("text");
+  var target = ev.target; // Set the variable for finding the first actually valid target
 
-  console.log("event:", ev);
-
-  if (ev.target.classList.contains("nondrop")) {
-    ev.target.insertAdjacentHTML(document.getElementById(data));
-  } else {
-    ev.target.appendChild(document.getElementById(data));
+  // If the actual target is not a valid drop location, search for one on the parent node.
+  // Repeat until a valid drop location is found
+  while (target.getAttribute("ondrop") != "drop(event)") {
+    // Set the parent node as the new target
+    target = target.parentNode;
   }
+
+  // Get the element that has to be transferred and append it to the bottom of the list of child elements of the actually valid target
+  target.appendChild(document.getElementById(data));
 }
