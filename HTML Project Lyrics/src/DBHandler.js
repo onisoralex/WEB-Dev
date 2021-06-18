4c59761f8544123794a3df8a43d8adcaa21cb632
Alexander Onisor <alexander.onisor@workflow.at>
2021-06-18 13:01:23 +0200

    DBHandler modification

--- HTML Project Lyrics/src/DBHandler.js
+++ HTML Project Lyrics/src/DBHandler.js
@@ -6,14 +6,26 @@ request.onsuccess = (event) => { db = event.target.result; };
 request.onupgradeneeded = (event) => {
   db = event.target.result; // Save the IDBDatabase interface
 
-  let objectStore = db.createObjectStore("chords", { keyPath: "chord_UID" }); // Create an objectStore for this database
+  const objectStore = db.createObjectStore("chords", { keyPath: "chord_UID" }); // Create an objectStore for this database
   objectStore.createIndex("name", "name", { unique: false });
+
+  objectStore.transaction.oncomplete = function(event) {
+    // Store values in the newly created objectStore.
+    let customerObjectStore = db.transaction("chords", "readwrite").objectStore("chords");
+    customerData.forEach(function(customer) {
+      customerObjectStore.add(customer);
+    });
+  };
 };
 
 // This is what our customer data looks like.
 const customerData = [
-  { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
-  { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+  {
+    ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com",
+  },
+  {
+    ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org",
+  },
 ];
 
 // https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API/Using_IndexedDB
