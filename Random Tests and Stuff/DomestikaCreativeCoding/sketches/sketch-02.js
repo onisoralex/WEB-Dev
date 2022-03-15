261c161eab333df93b8f577875e48893cdd16c15
onisoralex <onisor_alex@gmx.at>
2022-03-15 01:44:06 +0100

    Changes and experiments on sketch-02

--- Random Tests and Stuff/DomestikaCreativeCoding/sketches/sketch-02.js
+++ Random Tests and Stuff/DomestikaCreativeCoding/sketches/sketch-02.js
@@ -17,89 +17,146 @@ const rgbToHex = (rgb) => `#${numToHex(rgb.r)}${numToHex(rgb.g)}${numToHex(rgb.b
 
 const params = {
   exercise2: false,
-  animate: false,
+  animate: true,
   background: "#000000",
   fontColor: "#ffffff",
+  lineCap: "butt",
   posx: 0.5,
   posy: 0.5,
   radius: 0.3,
   dials: 40,
+  lineDial: false,
 };
+
+const animate = () => {
+  console.log("domestika");
+  requestAnimationFrame(animate); // Every time the browser is ready to animate a frame it willl call this function.
+};
+// animate();
 
 const settings = {
   dimensions: [1080, 1080],
-  animate: params.animate,
+  animate: true,
 };
 
 const degToRad = (degrees) => (degrees / 180) * Math.PI;
 
 const randomRange = (min, max) => Math.random() * (max - min) + min;
 
-const sketch = () => ({ context, width, height }) => {
-  context.fillStyle = params.background;
-  context.fillRect(0, 0, width, height);
+const sketch = ({ update, togglePlay }) => {
+  // window.addEventListener("click", () => update(settings));
 
-  context.fillStyle = params.fontColor;
+  (createPane = () => {
+    const pane = new Tweakpane.Pane();
+    let folder;
 
-  const cx = width * (params.exercise2 ? 0 : params.posx);
-  const cy = height * (params.exercise2 ? 0 : params.posy);
+    folder = pane.addFolder({ title: "General" });
+    folder.addInput(params, "exercise2").on("change", () => update());
+    folder.addInput(params, "animate").on("change", () => togglePlay());
+    folder.addInput(params, "background").on("change", () => update());
+    folder.addInput(params, "lineCap", { options: { butt: "butt", round: "round", square: "square" } }).on("change", () => update());
 
-  let x; let y;
+    folder = pane.addFolder({ title: "Art" });
+    folder.addInput(params, "fontColor").on("change", () => update());
+    folder.addInput(params, "posx", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "posy", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "radius", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "dials", { min: 0, max: 100, step: 1 }).on("change", () => update());
+    folder.addInput(params, "lineDial").on("change", () => update());
+  })();
 
-  const w = width * 0.01;
-  const h = height * 0.1;
+  return ({ context, width, height }) => {
+    context.fillStyle = params.background;
+    context.fillRect(0, 0, width, height);
 
-  const num = params.dials;
-  const radius = width * (params.exercise2 ? 0.7 : params.radius);
+    context.fillStyle = params.fontColor;
 
-  for (let i = 0; i < num; i++) {
-    const slice = math.degToRad(360 / num);
-    const angle = slice * i;
+    const cx = width * (params.exercise2 ? 0 : params.posx);
+    const cy = height * (params.exercise2 ? 0 : params.posy);
 
-    x = cx + radius * Math.sin(angle);
-    y = cy + radius * Math.cos(angle);
+    let x; let y;
 
-    context.save();
-    context.translate(x, y);
-    context.rotate(-angle);
-    context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
+    const w = width * 0.01;
+    const h = height * 0.1;
 
-    context.beginPath();
-    context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
-    context.fill();
-    context.restore();
+    const num = params.dials;
+    const radius = width * (params.exercise2 ? 0.7 : params.radius);
 
-    context.save();
-    context.translate(cx, cy);
-    context.rotate(-angle);
+    context.lineCap = params.lineCap;
 
-    context.lineWidth = random.range(5, 20);
-    context.strokeStyle = params.fontColor;
+    for (let i = 0; i < num; i++) {
+      const slice = math.degToRad(360 / num);
+      const angle = slice * i;
 
-    context.beginPath();
-    context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
-    context.stroke();
-    context.restore();
-  }
+      x = cx + radius * Math.sin(angle);
+      y = cy + radius * Math.cos(angle);
+
+      const wi = 500;
+      const he = 500;
+      context.lineWidth = 1;
+      context.strokeStyle = params.fontColor;
+      context.save();
+      context.translate(wi, he);
+      context.beginPath();
+      context.moveTo(0, -he);
+      context.lineTo(0, 1080 - he);
+      context.stroke();
+      context.beginPath();
+      context.moveTo(-wi, 0);
+      context.lineTo(1080 - wi, 0);
+      context.stroke();
+      context.restore();
+      context.save();
+      context.translate(wi + w, he + h);
+      context.beginPath();
+      context.moveTo(0, -he - h);
+      context.lineTo(0, 1080 - he - h);
+      context.stroke();
+      context.beginPath();
+      context.moveTo(-wi - h, 0);
+      context.lineTo(1080 - wi - w, 0);
+      context.stroke();
+      context.restore();
+      context.save();
+      context.translate(wi, he);
+
+      context.beginPath();
+      context.rect(-w * 0.5, -random.range(0, h * 0.5), w, h);
+      context.fill();
+      context.restore();
+
+      context.save();
+      context.translate(x, y);
+      context.rotate(-angle);
+      if (params.lineDial) {
+        // context.rotate(math.degToRad(90));
+        const xFactor = random.range(0.1, 2);
+        const yFactor = random.range(0.2, 0.5);
+        context.lineWidth = 5;
+        context.strokeStyle = params.fontColor;
+        context.beginPath();
+        context.moveTo(0, random.range(0, -h * 0.5));
+        context.lineTo(w, h);
+        context.stroke();
+      } else {
+        // context.scale(random.range(0.1, 2), random.range(0.2, 0.5));
+        context.beginPath();
+        context.rect(-w * 0.5, random.range(0, -h * 0.5), w, h);
+        context.fill();
+      }
+      context.restore();
+
+      context.save();
+      context.translate(cx, cy);
+      context.rotate(-angle);
+      context.lineWidth = random.range(5, 20);
+      context.strokeStyle = params.fontColor;
+      context.beginPath();
+      context.arc(0, 0, radius * random.range(0.7, 1.3), slice * random.range(1, -8), slice * random.range(1, 5));
+      context.stroke();
+      context.restore();
+    }
+  };
 };
 
-const createPane = () => {
-  const pane = new Tweakpane.Pane();
-  let folder;
-
-  folder = pane.addFolder({ title: "General" });
-  folder.addInput(params, "exercise2");
-  folder.addInput(params, "animate");
-  folder.addInput(params, "background");
-  // folder.addInput(params, "lineCap", { options: { butt: "butt", round: "round", square: "square" } });
-
-  folder = pane.addFolder({ title: "Art" });
-  folder.addInput(params, "fontColor");
-  folder.addInput(params, "posx", { min: 0, max: 1, step: 0.01 });
-  folder.addInput(params, "posy", { min: 0, max: 1, step: 0.01 });
-  folder.addInput(params, "radius", { min: 0, max: 1, step: 0.01 });
-  folder.addInput(params, "dials", { min: 0, max: 100, step: 1 });
-};
-
-createPane();
 canvasSketch(sketch, settings);
