cf51457c3b51ef0c784d9649a09a738171ed06a9
onisoralex <onisor_alex@gmx.at>
2022-03-30 23:29:38 +0200

    Added graph drawing feature and test graph

--- Random Tests and Stuff/DomestikaCreativeCoding/sketches/gaussian.js
+++ Random Tests and Stuff/DomestikaCreativeCoding/sketches/gaussian.js
@@ -13,14 +13,33 @@ const getRandomRangeArray = (n, min, max) => {
   return arr;
 };
 
+const drawLine = (c, tx, ty, bx, by, ex, ey, lw) => {
+  const context = c;
+  context.save();
+  context.translate(tx, ty);
+  context.beginPath();
+  context.lineWidth = lw;
+  context.moveTo(bx, by);
+  context.lineTo(ex, ey);
+  context.stroke();
+  context.restore();
+};
+
+const fx = (x) => x * x * x;
+
 const params = {
+  gaussian: false,
   animate: true,
   lineCap: "butt",
   background: "#000000",
-  scaleColor: "#ffffff",
   graphColor: "#ffffff",
-  posx: 0.5,
-  posy: 0.8,
+  plotColor: "#ffffff",
+  xOffset: 0.5,
+  yOffset: 0.5,
+  xGraphAxisMarkingsAmmount: 10,
+  yGraphAxisMarkingsAmmount: 10,
+  markingLength: 0.01,
+  precision: 1,
 };
 
 const settings = {
@@ -29,11 +48,8 @@ const settings = {
 };
 
 const sketch = ({
-  width, height, update, togglePlay,
+  update, togglePlay,
 }) => {
-  const xAxisLength = width * 0.8;
-  const yAxiyLength = height * 0.8;
-
   // Create all random values so they aren't created anew every frame
   const scaleXArr = getRandomRangeArray(params.maxNumOfElements, 0.1, 2);
   const scaleYArr = getRandomRangeArray(params.maxNumOfElements, 0.2, 0.5);
@@ -49,42 +65,76 @@ const sketch = ({
     let folder;
 
     folder = pane.addFolder({ title: "General" });
+    folder.addInput(params, "gaussian").on("change", () => update());
     folder.addInput(params, "animate").on("change", () => togglePlay());
     folder.addInput(params, "lineCap", { options: { butt: "butt", round: "round", square: "square" } }).on("change", () => update());
     folder.addInput(params, "background").on("change", () => update());
 
-    folder = pane.addFolder({ title: "Art" });
-    folder.addInput(params, "scaleColor").on("change", () => update());
+    folder = pane.addFolder({ title: "Graph" });
     folder.addInput(params, "graphColor").on("change", () => update());
-    folder.addInput(params, "posx", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
-    folder.addInput(params, "posy", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "xOffset", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "yOffset", { min: 0, max: 1, step: 0.01 }).on("change", () => update());
+
+    folder = pane.addFolder({ title: "Plot" });
+    folder.addInput(params, "plotColor").on("change", () => update());
+    folder.addInput(params, "markingLength", { min: 0, max: 0.02, step: 0.001 }).on("change", () => update());
+    folder.addInput(params, "xGraphAxisMarkingsAmmount", { min: 0, max: 30, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "yGraphAxisMarkingsAmmount", { min: 0, max: 30, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "precision", { min: 1, max: 10, step: 1 }).on("change", () => update());
   })();
 
   return ({ context, width, height }) => {
-    context.fillStyle = params.background;
-    context.fillRect(0, 0, width, height);
+    const graphAxisLength = width * 0.8;
+    const { xGraphAxisMarkingsAmmount } = params;
+    const { yGraphAxisMarkingsAmmount } = params;
+    const markingsLength = graphAxisLength * params.markingLength;
+    const graphMarkingsDistanceX = (graphAxisLength / xGraphAxisMarkingsAmmount) * 0.5;
+    const graphMarkingsDistanceY = (graphAxisLength / yGraphAxisMarkingsAmmount) * 0.5;
+    const ox = width * params.xOffset; // Origin x
+    const oy = height * (params.gaussian ? params.yOffset + 0.15 : params.yOffset); // Origin y
+    const graphLineWidth = 1;
+    const plotLineWidth = 2;
+    const graphLengthReduction = 1;
+    const xStart = -params.xGraphAxisMarkingsAmmount * graphLengthReduction;
+    const xEnd = params.xGraphAxisMarkingsAmmount * graphLengthReduction;
+    const { precision } = params;
+    const invPrecision = 1 / precision;
 
-    context.lineCap = params.lineCap;
-    context.fillStyle = params.scaleColor;
-    context.strokeStyle = params.scaleColor;
+    const c = context;
+    c.fillStyle = params.background;
+    c.fillRect(0, 0, width, height);
 
-    // Origin point
-    const ox = width * params.posx;
-    const oy = height * params.posy;
+    c.lineCap = params.lineCap;
+    c.fillStyle = params.graphColor;
+    c.strokeStyle = params.graphColor;
 
-    const graphWidth = 4;
+    // Move to Origin
+    c.save();
+    c.translate(ox, oy);
 
-    context.save();
-    context.translate(ox, oy);
-    context.beginPath();
-    context.lineWidth = graphWidth;
-    context.moveTo(-xAxisLength * 0.5, 0);
-    context.lineTo(xAxisLength * 0.5, 0);
-    context.stroke();
-    context.moveTo(0, -yAxiyLength * 0.9);
-    context.lineTo(0, yAxiyLength * 0.1);
-    context.stroke();
-    context.restore();
+    // Draw Graph
+    drawLine(c, 0, 0, -graphAxisLength * 0.5, 0, graphAxisLength * 0.5, 0, graphLineWidth);
+    drawLine(c, 0, 0, 0, -graphAxisLength * 0.5, 0, graphAxisLength * 0.1, graphLineWidth);
+    for (let i = 1; i <= xGraphAxisMarkingsAmmount; i++) {
+      drawLine(c, graphMarkingsDistanceX * i, 0, 0, -markingsLength, 0, markingsLength, graphLineWidth);
+      drawLine(c, -graphMarkingsDistanceX * i, 0, 0, -markingsLength, 0, markingsLength, graphLineWidth);
+    }
+    for (let i = 1; i <= yGraphAxisMarkingsAmmount; i++) {
+      drawLine(c, 0, graphMarkingsDistanceY * i, -markingsLength, 0, markingsLength, 0, graphLineWidth);
+      drawLine(c, 0, -graphMarkingsDistanceY * i, -markingsLength, 0, markingsLength, 0, graphLineWidth);
+    }
+
+    // Draw plot
+    c.fillStyle = params.plotColor;
+    c.strokeStyle = params.plotColor;
+    for (let i = xStart * precision; i < xEnd * precision; i++) {
+      const yValueStart = -fx(i * invPrecision); // Negative, because drawing direction is inversed compared to mathematics
+      const yValueEnd = -fx((i + 1) * invPrecision);
+      // drawLine(c, 0, 0, i * plotZoomX, yValueStart * plotZoomY, (i + 1) * plotZoomX, yValueEnd * plotZoomY, plotLineWidth);
+      drawLine(c, 0, 0, i * graphMarkingsDistanceX * invPrecision, yValueStart * graphMarkingsDistanceY, (i + 1) * graphMarkingsDistanceX * invPrecision, yValueEnd * graphMarkingsDistanceY, plotLineWidth);
+    }
+
+    c.restore();
   };
 };
 
