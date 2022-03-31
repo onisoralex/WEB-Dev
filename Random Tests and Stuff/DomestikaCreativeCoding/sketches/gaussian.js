abbb4e9c8e4ec5811d8b4c2fe2700981a911ccac
onisoralex <onisor_alex@gmx.at>
2022-03-31 23:52:49 +0200

    Finished gaussian.js

--- Random Tests and Stuff/DomestikaCreativeCoding/sketches/gaussian.js
+++ Random Tests and Stuff/DomestikaCreativeCoding/sketches/gaussian.js
@@ -25,10 +25,23 @@ const drawLine = (c, tx, ty, bx, by, ex, ey, lw) => {
   context.restore();
 };
 
-const fx = (x) => x * x * x;
-
+const fx2 = (x) => x * x;
+const fx3 = (x) => x * x * x;
+const gaussian = (x, params) => {
+  const divisor = Math.sqrt(2 * Math.PI * params.stddev ** 2);
+  const etermDividend = (x - params.mean) ** 2;
+  const etermDivisor = 2 * params.stddev ** 2;
+  const eterm = -etermDividend / etermDivisor;
+  return (1 / (divisor)) * (Math.E ** eterm);
+};
+const fx = (x, type, gaussianParams) => {
+  if (type === "x^2") return fx2(x);
+  if (type === "x^3") return fx3(x);
+  return gaussian(x, gaussianParams);
+};
 const params = {
-  gaussian: false,
+  gaussianGraph: true,
+  functionType: "gauss",
   animate: true,
   lineCap: "butt",
   background: "#000000",
@@ -39,7 +52,9 @@ const params = {
   xGraphAxisMarkingsAmmount: 10,
   yGraphAxisMarkingsAmmount: 10,
   markingLength: 0.01,
-  precision: 1,
+  precision: 10,
+  mean: 0,
+  stddev: 2,
 };
 
 const settings = {
@@ -65,7 +80,8 @@ const sketch = ({
     let folder;
 
     folder = pane.addFolder({ title: "General" });
-    folder.addInput(params, "gaussian").on("change", () => update());
+    folder.addInput(params, "gaussianGraph").on("change", () => update());
+    folder.addInput(params, "functionType", { options: { x2: "x^2", x3: "x^3", gauss: "gauss" } }).on("change", () => update());
     folder.addInput(params, "animate").on("change", () => togglePlay());
     folder.addInput(params, "lineCap", { options: { butt: "butt", round: "round", square: "square" } }).on("change", () => update());
     folder.addInput(params, "background").on("change", () => update());
@@ -78,27 +94,30 @@ const sketch = ({
     folder = pane.addFolder({ title: "Plot" });
     folder.addInput(params, "plotColor").on("change", () => update());
     folder.addInput(params, "markingLength", { min: 0, max: 0.02, step: 0.001 }).on("change", () => update());
-    folder.addInput(params, "xGraphAxisMarkingsAmmount", { min: 0, max: 30, step: 0.01 }).on("change", () => update());
-    folder.addInput(params, "yGraphAxisMarkingsAmmount", { min: 0, max: 30, step: 0.01 }).on("change", () => update());
+    folder.addInput(params, "xGraphAxisMarkingsAmmount", { min: 1, max: 30, step: 1 }).on("change", () => update());
+    folder.addInput(params, "yGraphAxisMarkingsAmmount", { min: 1, max: 30, step: 1 }).on("change", () => update());
     folder.addInput(params, "precision", { min: 1, max: 10, step: 1 }).on("change", () => update());
+    folder.addInput(params, "mean", { min: -10, max: 10, step: 1 }).on("change", () => update());
+    folder.addInput(params, "stddev", { min: 0.1, max: 2, step: 0.01 }).on("change", () => update());
   })();
 
   return ({ context, width, height }) => {
     const graphAxisLength = width * 0.8;
-    const { xGraphAxisMarkingsAmmount } = params;
-    const { yGraphAxisMarkingsAmmount } = params;
+    const xGraphAxisMarkingsAmmount = params.gaussianGraph ? 5 : params.xGraphAxisMarkingsAmmount;
+    const yGraphAxisMarkingsAmmount = params.gaussianGraph ? 1 : params.yGraphAxisMarkingsAmmount;
     const markingsLength = graphAxisLength * params.markingLength;
     const graphMarkingsDistanceX = (graphAxisLength / xGraphAxisMarkingsAmmount) * 0.5;
     const graphMarkingsDistanceY = (graphAxisLength / yGraphAxisMarkingsAmmount) * 0.5;
     const ox = width * params.xOffset; // Origin x
-    const oy = height * (params.gaussian ? params.yOffset + 0.15 : params.yOffset); // Origin y
+    const oy = height * (params.gaussianGraph ? params.yOffset + 0.15 : params.yOffset); // Origin y
     const graphLineWidth = 1;
     const plotLineWidth = 2;
-    const graphLengthReduction = 1;
-    const xStart = -params.xGraphAxisMarkingsAmmount * graphLengthReduction;
-    const xEnd = params.xGraphAxisMarkingsAmmount * graphLengthReduction;
+    const graphLengthReductionX = 1;
+    const xAxisLength = xGraphAxisMarkingsAmmount * graphLengthReductionX;
     const { precision } = params;
     const invPrecision = 1 / precision;
+    const negativeAxisScaleY = params.gaussianGraph ? 0.2 : 1;
+    const gaussianParams = { mean: params.mean, stddev: params.stddev };
 
     const c = context;
     c.fillStyle = params.background;
@@ -114,22 +133,24 @@ const sketch = ({
 
     // Draw Graph
     drawLine(c, 0, 0, -graphAxisLength * 0.5, 0, graphAxisLength * 0.5, 0, graphLineWidth);
-    drawLine(c, 0, 0, 0, -graphAxisLength * 0.5, 0, graphAxisLength * 0.1, graphLineWidth);
+    drawLine(c, 0, 0, 0, -graphAxisLength * 0.5, 0, graphAxisLength * 0.5 * negativeAxisScaleY, graphLineWidth);
     for (let i = 1; i <= xGraphAxisMarkingsAmmount; i++) {
       drawLine(c, graphMarkingsDistanceX * i, 0, 0, -markingsLength, 0, markingsLength, graphLineWidth);
       drawLine(c, -graphMarkingsDistanceX * i, 0, 0, -markingsLength, 0, markingsLength, graphLineWidth);
     }
     for (let i = 1; i <= yGraphAxisMarkingsAmmount; i++) {
-      drawLine(c, 0, graphMarkingsDistanceY * i, -markingsLength, 0, markingsLength, 0, graphLineWidth);
       drawLine(c, 0, -graphMarkingsDistanceY * i, -markingsLength, 0, markingsLength, 0, graphLineWidth);
+      if (i <= yGraphAxisMarkingsAmmount * negativeAxisScaleY) {
+        drawLine(c, 0, graphMarkingsDistanceY * i, -markingsLength, 0, markingsLength, 0, graphLineWidth);
+      }
     }
 
     // Draw plot
     c.fillStyle = params.plotColor;
     c.strokeStyle = params.plotColor;
-    for (let i = xStart * precision; i < xEnd * precision; i++) {
-      const yValueStart = -fx(i * invPrecision); // Negative, because drawing direction is inversed compared to mathematics
-      const yValueEnd = -fx((i + 1) * invPrecision);
+    for (let i = -xAxisLength * precision; i < xAxisLength * precision; i++) {
+      const yValueStart = -fx(i * invPrecision, params.functionType, gaussianParams); // Negative, because drawing direction is inversed compared to mathematics
+      const yValueEnd = -fx((i + 1) * invPrecision, params.functionType, gaussianParams);
       // drawLine(c, 0, 0, i * plotZoomX, yValueStart * plotZoomY, (i + 1) * plotZoomX, yValueEnd * plotZoomY, plotLineWidth);
       drawLine(c, 0, 0, i * graphMarkingsDistanceX * invPrecision, yValueStart * graphMarkingsDistanceY, (i + 1) * graphMarkingsDistanceX * invPrecision, yValueEnd * graphMarkingsDistanceY, plotLineWidth);
     }
