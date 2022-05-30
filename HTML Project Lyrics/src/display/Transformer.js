1eefedc4c5ea54754fa2ad5c59ef1bf958f6d71c
onisoralex <onisor_alex@gmx.at>
2022-05-31 00:44:07 +0200

    Finished songtext displaying

--- HTML Project Lyrics/src/display/Transformer.js
+++ HTML Project Lyrics/src/display/Transformer.js
@@ -1,42 +1,64 @@
 // Replace the space (and eventuall multiple trailing spaces) with a nonbreaking space
 // It's the only solution to create fixed spaces. Will cause nonbreaking points in text.
 // Could eventually be fixed with usage of CSS Grid-View
-const removeTrailingSpacesAddNBSP = (text) => `${text.trimEnd()}&nbsp;`;
+const removeTrailingSpaces = (text) => `${text.trimEnd()}`;
 
-const removeLeadingSpacesAddNBSP = (text) => `&nbsp;${text.trimStart()}`;
+const removeLeadingSpaces = (text) => `${text.trimStart()}`;
 
-const isLastElement = (i, e) => (i + 1) === e.length;
+const isLastElement = (i, e) => i === e.length - 1;
 
-const makeLine = (chordsLine, lyricsLine) => {
+const makeLine = (chordsLine, lyricsLine, readableChords) => {
   let text = "";
+
+  const sp = "&nbsp;";
+  const trisp = "&nbsp;&nbsp;&nbsp;";
+  let preChord = "";
+  const postChord = readableChords ? trisp : sp;
 
   for (let i = 0; i < chordsLine.length; i++) {
     const a = chordsLine[i].getChordAsText();
 
-    const chordStart = chordsLine[i].getPosition();
-    const chordEnd = (isLastElement(i, chordsLine)) ? lyricsLine.length : chordsLine[i + 1].getPosition();
-    let preChord = "";
-    const postChord = "&nbsp;";
-    let b = lyricsLine.substring(chordStart, chordEnd);
-    console.log(i, b);
-    // If a block ends with a space remove trailing spaces and replace it with a non-breaking space
-    if (b.at(b.length - 1) === " ") b = removeTrailingSpacesAddNBSP(b);
-    if (b.at(0) === " ") b = removeLeadingSpacesAddNBSP(b);
-    if (b === "") {
-      b = "&nbsp;";
-      preChord = (chordStart === 0) ? "&nbsp;" : "&nbsp;&nbsp;&nbsp;";
+    if (lyricsLine !== undefined) {
+      const chordStart = chordsLine[i].getPosition();
+      const chordEnd = (isLastElement(i, chordsLine)) ? lyricsLine.length : chordsLine[i + 1].getPosition();
+      let b = lyricsLine.substring(chordStart, chordEnd);
+
+      // If a block ends with a space remove trailing spaces and replace it with a non-breaking space
+      if (b.at(-1) === " ") b = removeTrailingSpaces(b).concat("&nbsp;");
+
+      // If a block starts with a space it should become a non-breaking space - relevant for Chords between words and at start of sentences
+      if (b.at(0) === " ") {
+        b = removeLeadingSpaces(b);
+        b = `${chordStart === 0 ? trisp : sp}${b}`;
+      }
+
+      // Offset the counter and display the text only
+      // The first chord could start in the iddle of the sentence - first part of sentence should still be displayed
+      if (i === 0 && chordStart !== 0) text += `<c-b t="">${lyricsLine.substring(0, chordStart)}</c-b>`;
+
+      // If a block is empty it could be a chord after the end of a sentence (chordchanges without singing) - make some spaces between the chords
+      // Undefined is for the instrumentals that have no lyrics
+      if (b === "" || b === undefined) {
+        b = trisp;
+        preChord = trisp;
+      }
+
+      text += `<c-b t="${preChord}${a}${postChord}">${b}</c-b>`;
+    } else {
+      // Special treatment for Solo and Intrumentals
+      text += `<c-b t="${a}${trisp}" class="song-text-no-line-height">${sp}</c-b>`;
     }
-    text += `<c-b t="${preChord}${a}${postChord}">${b}</c-b>`;
   }
+
   text += "<br>";
-  console.log(text);
+  // console.log(text);
 
   return text;
 };
 
-const makeText = (parts) => {
+const makeText = (parts, readableChords) => {
   let text = "";
-  for (let i = 0; i < 2; i++) {
+  for (let i = 0; i < parts.length; i++) {
     const part = parts[i];
     const { codedChords, lyrics } = part;
 
@@ -44,8 +66,10 @@ const makeText = (parts) => {
     text += `<div class="js-song__part-name--visible"><strong>${part.name}</strong><br></div>`;
     for (let j = 0; j < codedChords.length; j++) {
       const chordsLine = codedChords[j];
-      const lyricsLine = (typeof lyrics === "undefined") ? "" : lyrics[j];
-      text += makeLine(chordsLine, lyricsLine);
+      // The lyrics line still has to be undefined even if the part doesn't have lyrics
+      // Else it will be interpreted as chords at the end of a sentence that have no text
+      const lyricsLine = (typeof lyrics === "undefined") ? undefined : lyrics[j];
+      text += makeLine(chordsLine, lyricsLine, readableChords);
     }
     text += `</div>`;
   }
@@ -53,7 +77,7 @@ const makeText = (parts) => {
   return text;
 };
 
-const createText = (song) => {
+const createText = (song, readableChords) => {
   let display = "";
 
   display += `<div id="js-song__title"><h2>${song.getTitle()}</h2></div>`;
@@ -61,8 +85,8 @@ const createText = (song) => {
   display += `<div id="js-song__defaultKey"><strong>Default Key:</strong> ${song.getDefaultKey()}</div>`;
   display += `<div id="js-song__currentKey"><strong>Current Key:</strong> ${song.getKey()}</div>`;
   display += `<div id="js-song__tempo"><strong>Tempo:</strong> ${song.getTempo()}</div>`;
-  display += `<div id="js-song__text"><br>${makeText(song.songParts)}<br></div>`;
-  display += `<div id="js-song__defaultStructure"><strong>Default Structure:</strong> ${song.getDefaultStructure()}<strong></div>`;
+  display += `<div id="js-song__defaultStructure"><strong>Default Structure:</strong> ${song.getDefaultStructure()}</div>`;
+  display += `<div id="js-song__text"><br>${makeText(song.songParts, readableChords)}<br></div>`;
 
   return display;
 };
--- HTML Project Lyrics/src/main.js
+++ HTML Project Lyrics/src/main.js
@@ -8,10 +8,11 @@ window.parseSongFromText = parseSongFromText;
 window.start = () => {
   const completeSongTextNode = document.getElementById("song-text__input");
   const ouputNode = document.getElementById("song-text__output"); // Gets the Place where the Lyrics should go in the HTML
+  const readableChords = false;
 
   const song = parseSongFromText(completeSongTextNode.innerHTML);
   console.log("Original");
   console.log(song);
 
-  ouputNode.innerHTML = createText(song);
+  ouputNode.innerHTML = createText(song, readableChords);
 };
