a7f4cfdb7f39f62e4da3ed2a4923783de6c74f37
onisoralex <onisor_alex@gmx.at>
2022-08-14 23:09:20 +0200

    Moved Project Lyrics in new GitHub project: project-lyrics

--- HTML Project Lyrics/src/ImportExport/DBExport.js
+++
@@ -1,9 +0,0 @@
-import { createFileDownloadLink } from "../Utilities/Utils.js";
-
-const exportToDBFile = (obj, elementID) => {
-  const element = document.getElementById(elementID);
-  const filename = "export.JSON";
-  createFileDownloadLink(obj, filename, element);
-};
-
-export { exportToDBFile };
