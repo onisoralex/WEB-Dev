8981dfe141c34970e5289c1aa577931f00499afb
onisoralex <onisor_alex@gmx.at>
2023-10-30 01:59:05 +0100

    Added mor information in the async/awailt test

--- Random Tests and Stuff/AsyncAwaitTests/main.js
+++ Random Tests and Stuff/AsyncAwaitTests/main.js
@@ -1,5 +1,12 @@
-window.openAndDisplayFile = (eventNode, fileTarget) => {
-  const input = eventNode.target; // Takes Information from the HTML element that executed this Script
+window.openAndDisplayFile = async (eventNode, fileTarget) => {
+  await alert("testtext"); // Alert wird angezeigt, aber nichts passiert, bis man auf "OK" heklickt hat.
+  await readfile(eventNode, fileTarget); // Readfile is executed but aparently the code still continues with the next alert
+  alert("finished"); // This alert is executed and ONLY AFTER closing it the text will be displayed
+};
+
+
+const readfile = (_eventNode, fileTarget) => {
+  const input = _eventNode.target; // Takes Information from the HTML element that executed this Script
   const fileReader = new FileReader(); // Create a new File Reader
   const targetNode = fileTarget;
 
