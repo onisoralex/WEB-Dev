456540f10cc998cb14dec1eadef99821653d373f
onisoralex <onisor_alex@gmx.at>
2023-11-02 05:29:11 +0100

    Finalized main functionality of OrderReady app

--- Random Tests and Stuff/OrderReady/main.js
+++ Random Tests and Stuff/OrderReady/main.js
@@ -1,5 +1,43 @@
 import { drag, drop, allowDrop } from "./draganddrop.js";
 
+const addNumberToDisplay = (n) => {
+  const divElement = document.createElement("div");
+  divElement.id = `number${n}`;
+  divElement.setAttribute("class", "drag");
+  divElement.setAttribute("draggable", "true");
+  divElement.setAttribute("ondragstart", "drag(event)");
+
+  const labelElement = document.createElement("label");
+  labelElement.setAttribute("class", "ordernumberblock");
+  labelElement.innerHTML = n;
+
+  document.getElementById("orderdisplay").appendChild(divElement);
+  divElement.appendChild(labelElement);
+};
+
+const add = () => {
+  const el = document.getElementById("numberinput");
+
+  addNumberToDisplay(el.value);
+
+  el.value = +el.value + 1;
+};
+
+const validateNumber = (event) => {
+  const ev = event;
+  const n = +ev.target.value;
+  const lo = +ev.target.min;
+  const hi = +ev.target.max;
+
+  if (n <= lo) {
+    ev.target.value = lo;
+  } else if (n >= hi) {
+    ev.target.value = hi;
+  }
+};
+
 window.drag = drag;
 window.drop = drop;
 window.allowDrop = allowDrop;
+window.add = add;
+window.validateNumber = validateNumber;
